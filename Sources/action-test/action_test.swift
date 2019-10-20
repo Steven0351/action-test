da84030c203dddca61cdf9a8a44998901bb81baf
Steven Sherry <steven.sherry@affinityforapps.com>
2019-10-19 23:26:27 -0500

    CMON

--- Sources/action-test/action_test.swift
+++ Sources/action-test/action_test.swift
@@ -1,5 +1,5 @@
 /// A type for things so I can test this...is embarrasing...it would be nice to be able to run this locally?
 struct action_test {
-    /// A property to contain hello world text!!!!!!
+    /// A property to contain hello world text!!!!
     var text = "Hello, World!"
 }
