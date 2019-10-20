16721fbedb19e53dcc46dee18842f70f81e739e0
Steven Sherry <steven.sherry@affinityforapps.com>
2019-10-19 22:31:15 -0500

    JEEZ.

--- Sources/action-test/action_test.swift
+++ Sources/action-test/action_test.swift
@@ -1,4 +1,4 @@
-/// A type for things so I can test this
+/// A type for things so I can test this...is embarrasing
 struct action_test {
     /// A property to contain hello world text!!
     var text = "Hello, World!"
