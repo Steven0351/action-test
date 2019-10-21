7a2759afa25cac3598f6413bfce5d907f6d2cba7
Steven Sherry <steven.sherry@affinityforapps.com>
2019-10-21 13:44:07 -0500

    Try using jazzy_args, update action_test to ActionTest

--- Sources/action-test/action_test.swift
+++ Sources/action-test/action_test.swift
@@ -1,5 +1,5 @@
 /// A type for things so I can test this...is embarrasing...it would be nice to be able to run this locally?
-public struct action_test {
+public struct ActionTest {
     /// A property to contain hello world text!!!!
     public var text = "Hello, World!"
 }
